107d3d63bb90233dd15777de9316fb15b6a67fa5
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-01-11 07:12:04 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.jsx
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import sound from './assets/music/music.mp3';
 import logo from './assets/logo.png';
